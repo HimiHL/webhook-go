04552850341dd290f362abbc862071d1e454786a
黄海林 <10000@cxzfb.cn>
2018-11-15 11:15:31 +0800

    移除了不支持的windows版本，完善了README，添加了信号

--- README.md
+++ README.md
@@ -1,55 +1,76 @@
-# Go编写的自动部署
+# 前言
 
-主要用于小项目托管在Gitee.com和Coding.net中的项目进行自动部署，将 `hook` 或者 `hook.exe` 运行，就可以运行一个接收hook的HTTP服务器
+主要用于小项目托管在[码云](http://gitee.com)和[Coding](http://coding.net)中的项目进行自动部署，将 `hook`  运行，就可以运行一个轻型的HTTP服务器
 
-可多项目部署，如有多个项目，增加多个 `.json` 文件配置即可
+可多项目部署，如有多个项目，增加多个 `.json` 文件配置即可，__不需要重启hook进程__
 
-#### 启动项目
 
-    ./hook -port=8888
+# 支持
 
-#### 访问路由
+- [x] 支持Linux平台下部署
+- [x] 支持Git协议，即: git@coding.net:user/project.git
+- [ ] 不支持Http协议，即: https://git.coding.net/user/project.git
+- [x] 支持自定义Shell脚本
 
-    Gitee：`/gitee`
+# 命令列表
 
-    Coding: `/coding`
+```shell
+hook             # 启动服务器，-p 缺省，默认 `7442`
+hook -p 7442     # 启动监听端口为7442的服务器
 
-    例如：http://0.0.0.0:8888/gitee
+hook -h          # 帮助信息
+hook -s reload   # 重启
+hook -s stop     # 强行停止 SIGKILL
+```
 
-### 文件说明
+# 路由列表
+
+* /gitee
+
+解析[码云](http://gitee.com)的webhook通知，支持 `ContentType: application/json` 
+
+* /coding
+  
+解析[Coding](http://coding.net)的webhook通知，支持V2版本的通知、`ContentType: application/json`
+
+
+# 文件说明
 
 *  **git.sh**
 
-    该文件是用于部署的，传的三个参数分别是 `项目在本机的目录` `项目的别名origin/master` `项目分支`
+该文件是用于部署的，传的三个参数分别是 `项目在本机的目录` `项目的别名origin/master` `项目分支`
 
-    例如：
+也可以在**文件末尾**追加其他处理: 比如正式版发布时需要批量替换URL等操作
 
-    ```
+#### 可手动执行
 
-     ./git.sh /home/wwwroot/abc origin/master master
-
-    ```
+```shell
+./git.sh /home/wwwroot/abc origin/master master
+```
 
 *  **hook.go**
 
-    源码
+源码
 
 
 * **user.project.branch.json**
 
-    ###### 例如你的coding用户名为test，项目名称为 himiweb，部署分支为 master， 则文件命名为： `test.himiweb.master.json`
+例如你的coding用户名为**test**，项目名称为 himiweb，部署分支为 master， 则文件命名为： `test.himiweb.master.json`
 
-    JSON配置文件，分别代指
+#### JSON配置解析
 
-    ```
-    {
-        "path": "项目在本机的目录，如 /home/wwwroot/abc",
-        "head": "项目的别名，如origin/master",
-        "password": "hook的验证密码"
-    }
-    ```
+```json
+{
+    "path": "项目在本机的目录，如 /home/wwwroot/abc",
+    "head": "项目的别名，如origin/master",
+    "password": "hook的验证密码"
+}
+```
 
-* **hook && hook.exe**
+* **hook**
 
-    两个文件分别是linux和windows下的可执行二进制文件
-    ##### 如需更多系统版本，请自行运行 go build hook.go 打包
+Linux下可执行的二进制文件
+
+* **log**
+  
+系统运行时生成的Log文件
--- hook.go
+++ hook.go
@@ -2,16 +2,47 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
 	"strings"
+	"syscall"
 
 	"github.com/bitly/go-simplejson"
 	daemon "github.com/sevlyar/go-daemon"
 )
+
+var (
+	help   = flag.Bool("h", false, "The Help")
+	signal = flag.String("s", "", "send `signal` to a master process: stop, reload")
+	port   = flag.String("p", "7442", "HTTP Server Port, Default `7442`")
+)
+
+func init() {
+	flag.Usage = usage
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, `Webhook version: webhook/1.0.0
+	Usage: hook [-h] [-s signal] [-p port]
+
+	Options: 
+	`)
+	flag.PrintDefaults()
+}
+
+func killHandler(sig os.Signal) error {
+	log.Println("已停止运行")
+	return nil
+}
+
+func reloadHandler(sig os.Signal) error {
+	log.Println("服务器重载成功")
+	return nil
+}
 
 /**
  * main主函数入口
@@ -19,6 +50,19 @@ import (
  */
 func main() {
 
+	// 命令行的参数获取
+	flag.Parse()
+	if *help {
+		flag.Usage()
+	} else {
+		deamonHTTP()
+	}
+
+}
+
+func deamonHTTP() {
+	daemon.AddCommand(daemon.StringFlag(signal, "stop"), syscall.SIGKILL, killHandler)
+	daemon.AddCommand(daemon.StringFlag(signal, "reload"), syscall.SIGHUP, reloadHandler)
 	cntxt := &daemon.Context{
 		PidFileName: "pid",
 		PidFilePerm: 0644,
@@ -29,9 +73,19 @@ func main() {
 		Args:        os.Args,
 	}
 
+	if len(daemon.ActiveFlags()) > 0 {
+		d, err := cntxt.Search()
+		if err != nil {
+			log.Fatalln("信号发送失败:", err)
+		}
+		daemon.SendCommands(d)
+		return
+	}
+
 	d, err := cntxt.Reborn()
 	if err != nil {
-		log.Fatal("Unable to run: ", err)
+		log.Fatal("无法启动服务器: ", err)
+		return
 	}
 	if d != nil {
 		return
@@ -41,20 +95,21 @@ func main() {
 	log.Print("- - - - - - - - - - - - - - -")
 	log.Print("进程运行")
 
-	// 设置端口
-	var port string
-	flag.StringVar(&port, "port", "7442", "set Http Server Port, default `7442`")
-	flag.Parse()
+	go serveHTTP()
 
-	serveHTTP(port)
+	err = daemon.ServeSignals()
+	if err != nil {
+		log.Println("Error:", err)
+	}
+	log.Println("daemon terminated")
 }
 
-func serveHTTP(port string) {
+func serveHTTP() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/gitee", gitee)
 	mux.HandleFunc("/coding", coding)
 	mux.HandleFunc("/", index)
-	log.Fatalln(http.ListenAndServe(":"+port, mux))
+	log.Fatalln(http.ListenAndServe(":"+*port, mux))
 }
 
 /**
